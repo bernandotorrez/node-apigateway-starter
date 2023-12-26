d89b176c68dfc80e066a7feb8f4a76038607aa8a
Bernand <mail.bernand@gmail.com>
2023-12-26 13:47:49 +0700

    add proxy middleware : only can access by api gateway

--- todo-service/middleware/proxy.js
+++ todo-service/middleware/proxy.js
@@ -1,7 +1,6 @@
 function auth(req, res, next) {
     const validApiGatewayIP = ['::ffff:127.0.0.1'];
     const remoteAddress = req.connection.remoteAddress;
-    console.log(remoteAddress);
 
     if(validApiGatewayIP.includes(remoteAddress)) {
         next();
