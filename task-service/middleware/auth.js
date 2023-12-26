76248896e8b75f60e7a1978cba02a227d8173362
Bernand <mail.bernand@gmail.com>
2023-12-26 15:21:07 +0700

    refactor

--- task-service/middleware/auth.js
+++ task-service/middleware/auth.js
@@ -1,16 +1,12 @@
 const globalFunction = require('../utils/globalFunction');
 const jwt = require('jsonwebtoken');
+const AuthenticationError = require('../exceptions/AuthenticationError');
 
 function auth(req, res, next) {
     const token = globalFunction.check_null(req.header('X-Auth-Token'));
 
     if(token == '-') {
-        res.status(401).send({
-            code: 401,
-            status: 'ERROR',
-            message: 'Token is Empty',
-            data: null,
-        });
+        throw new AuthenticationError('Token is Empty');
     } else {
         try {
             const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
@@ -18,21 +14,10 @@ function auth(req, res, next) {
             next();
         } catch(ex) {
             if(ex instanceof jwt.TokenExpiredError) {
-                res.status(401).send({
-                    code: 401,
-                    status: 'ERROR',
-                    message: 'Token is Expired',
-                    data: null,
-                });
+                throw new AuthenticationError('Token is Expired');
             } else {
-                res.status(401).send({
-                    code: 401,
-                    status: 'ERROR',
-                    message: 'Token is Invalid',
-                    data: null,
-                });
+                throw new AuthenticationError('Token is Invalid');
             }
-            
         }
     }
 }
