95c6fb3fabacaa438725ed98237a427e7337d404
Bernand <mail.bernand@gmail.com>
2023-12-26 14:46:23 +0700

    refactor code with exception

--- task-service/middleware/proxy.js
+++ task-service/middleware/proxy.js
@@ -1,17 +1,13 @@
+const AuthorizationError = require('../exceptions/AuthorizationError');
+
 function auth(req, res, next) {
     const validApiGatewayIP = ['::ffff:127.0.0.1'];
     const remoteAddress = req.connection.remoteAddress;
-    console.log(remoteAddress);
 
     if(validApiGatewayIP.includes(remoteAddress)) {
         next();
     } else {
-        res.status(403).send({
-            code: 403,
-            status: 'ERROR',
-            message: 'Please HIT with API Gateway',
-            data: null,
-        });
+        throw new AuthorizationError('Please HIT with API Gateway')
     }
 }
 
--- todo-service/middleware/proxy.js
+++ todo-service/middleware/proxy.js
@@ -1,3 +1,5 @@
+const AuthorizationError = require('../exceptions/AuthorizationError');
+
 function auth(req, res, next) {
     const validApiGatewayIP = ['::ffff:127.0.0.1'];
     const remoteAddress = req.connection.remoteAddress;
@@ -5,12 +7,7 @@ function auth(req, res, next) {
     if(validApiGatewayIP.includes(remoteAddress)) {
         next();
     } else {
-        res.status(403).send({
-            code: 403,
-            status: 'ERROR',
-            message: 'Please HIT with API Gateway',
-            data: null,
-        });
+        throw new AuthorizationError('Please HIT with API Gateway')
     }
 }
 
